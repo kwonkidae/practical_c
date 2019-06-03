77eb3b10ce5a980b86e52fd63ab1cbc5ca1f3648
kwon <kkdosk@naver.com>
2019-06-03 10:13:48 +0900

    add

--- ch11/and/and.c
+++ ch11/and/and.c
@@ -3,10 +3,13 @@
 int main() {
   int i1, i2;
 
-  i1 = 4;
-  i2 = 2;
+  i1 = 3;
+  i2 = 3;
 
   if ((i1 != 0) && (i2 != 0))
     printf("Both are not zero\n");
+
+  if (i1 && i2)
+    printf("Both are not zero");
   return 0;
 }
